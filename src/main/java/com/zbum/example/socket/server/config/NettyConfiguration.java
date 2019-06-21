9f24dadaff27a5bd1cbda215a6e59555dfa3873c
ganyun <2211796911@qq.com>
2019-06-21 12:11:50 +0800

    init

---
+++ src/main/java/com/zbum/example/socket/server/config/NettyConfiguration.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.zbum.example.socket.server.config;
+
+import com.zbum.example.socket.server.netty.ChannelRepository;
+import com.zbum.example.socket.server.netty.handler.SimpleChatChannelInitializer;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.net.InetSocketAddress;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableConfigurationProperties(NettyProperties.class)
+public class NettyConfiguration {
+
+    private final NettyProperties nettyProperties;
+
+    @Bean(name = "serverBootstrap")
+    public ServerBootstrap bootstrap() {
+        ServerBootstrap b = new ServerBootstrap();
+        b.group(bossGroup(), workerGroup())
+                .channel(NioServerSocketChannel.class)
+                .handler(new LoggingHandler(LogLevel.DEBUG))
+                .childHandler(somethingChannelInitializer);
+        b.option(ChannelOption.SO_BACKLOG, nettyProperties.getBacklog());
+        return b;
+    }
+
+    @Autowired
+    private SimpleChatChannelInitializer somethingChannelInitializer;
+
+    @Bean(destroyMethod = "shutdownGracefully")
+    public NioEventLoopGroup bossGroup() {
+        return new NioEventLoopGroup(nettyProperties.getBossCount());
+    }
+
+    @Bean(destroyMethod = "shutdownGracefully")
+    public NioEventLoopGroup workerGroup() {
+        return new NioEventLoopGroup(nettyProperties.getWorkerCount());
+    }
+
+    @Bean
+    public InetSocketAddress tcpSocketAddress() {
+        return new InetSocketAddress(nettyProperties.getTcpPort());
+    }
+
+    @Bean
+    public ChannelRepository channelRepository() {
+        return new ChannelRepository();
+    }
+}
